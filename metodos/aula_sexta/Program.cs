444be1adfe813d543e595848629b3ebe0a15ec30
jorgeAraripina <113356403+jorgeAraripina@users.noreply.github.com>
2022-09-30 10:31:42 -0300

    Add files via upload

---
+++ metodos/aula_sexta/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_sexta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("bem vindo pessaol");
+                Frase();
+            Console.WriteLine("aqui nova forma" + Soma(100, 10));
+            Console.WriteLine("aqui é mulpliação:"+Multiplica(100,10));
+            Console.WriteLine("aqui nova forma" + Divisao(100, 10));
+            Console.WriteLine("aqui nova forma" + Subtração(100, 10));
+
+
+
+        }
+        static void Frase()
+        {
+            Console.WriteLine("olá pessaol"+"aula sobre metodo ");
+
+        } 
+        static double Soma(int n1,int n2)
+        {
+            double resultado;
+            resultado = n1 + n2;
+            return resultado;
+        }
+        static double Multiplica(int n1,int n2)
+        {
+            double resultado;
+            resultado = n1 * n2;
+            return resultado;
+
+        }
+        static double Divisao(int n1 ,int n2)
+        {
+            double resultado;
+            resultado=n1 / n2;
+            return resultado;
+
+        }
+        static double Subtração(int n1,int n2)
+        {
+            double resultado;
+            resultado=n1-n2;
+            return resultado;
+        }
+    }
+
+}
